ae61fe98ebc15b212ca6a8cb0ff9f5b8caac3a46
Michael Filzwieser <michaelf@titaniumford.ca>
2026-04-28 10:46:45 -0700

    regenerate favicon and apple touch icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon.ico",
-    apple: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
   },
 };
 
